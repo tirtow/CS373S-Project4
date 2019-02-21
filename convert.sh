301ff9950b1b78983077a65e3f23bdfe166418ae
tirtow <mtirtowidjojo@gmail.com>
2019-02-21 15:58:34 -0600

    Add how to run scripts

--- convert.sh
+++ convert.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+## To Run:
+# MDELite8.jar must be on the CLASSPATH as well as the "build/classes/"
+# directory from the directory with this script.
+# Takes a single argument (lead name of a violet class file "X" for "X.class.violet"
+
 java Violett.ClassParser $1.class.violet
 java Violett.ClassConform $1.vpl.pl
 java toschema.ToOOSchema $1.vpl.pl
